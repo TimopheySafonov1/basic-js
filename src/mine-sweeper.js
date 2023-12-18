b99f843b54b616d082257141f99ccb5d86cc5d84
TimopheySafonov1 <earth2002ocean@gmail.com>
2023-12-18 15:12:41 +0100

    feat: implement simple-chain

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const columns = matrix[0].length;
+  const rows = matrix.length;
+  const result = [];
+  for (let i = 0; i < rows; i++){
+    result.push([]);
+    for (let j = 0; j < columns; j++){
+      let count = 0;
+      for (let x = -1; x <=1; x++){
+        for(let y = -1; y <= 1; y++){
+          if(i + x >= 0 && x + i < rows && j + y >= 0 && j + y < columns){
+            if (x !== 0 || y !== 0)
+            if(matrix[i+x][j+y]){
+              count++;
+            }
+          }
+        }
+      }
+      result[i].push(count);
+    }
+  }
+  return result;
 }
 
 module.exports = {
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -3,9 +3,9 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class DepthCalculator with method calculateDepth
  * that calculates deoth of nested array
- * 
+ *
  * @example
- * 
+ *
  * const depthCalc = new DepthCalculator();
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
@@ -13,9 +13,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    if (!Array.isArray(arr)) return false;
+    let maxdepth = 0;
+    for (let i = 0; i < arr.length; i++) {
+      let depth = this.calculateDepth(arr[i]);
+      if (depth > maxdepth){
+        maxdepth = depth;
+      }
+    }
+
+    return maxdepth + 1;
   }
 }
 
